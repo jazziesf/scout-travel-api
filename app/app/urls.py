d4f541f2d3cd8b463660805ab0bb9c4f910499d3
Jazz <jazziesf21@yahoo.com>
2019-01-08 11:54:27 -0800

    added working list tags endpoints

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,7 +18,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/user/', include('user.urls'))
+    path('api/user/', include('user.urls')),
+    path('api/pin/', include('pin.urls')),
+
+
     # this call looks at the user app and extends to
     # the file in user to urls if it matching the path called
     # it will get passed to url path that is called
